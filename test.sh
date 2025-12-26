1792d124fa5dd85720445f866c82342213b0ec7d
Your Name <you@example.com>
2025-12-26 10:12:26 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES="0" \
 python test.py \
---checkpoint "/home/vuedale/Desktop/sam2.1/Fine-Tuning-SAM2-UNet-for-Breast-Image-Segmentation-Without-Pectoral-Muscle-and-Artifacts/checkpoints/best_model_epoch-12.pth" \
---test_image_path "/home/vuedale/Desktop/sam2.1/Fine-Tuning-SAM2-UNet-for-Breast-Image-Segmentation-Without-Pectoral-Muscle-and-Artifacts/sam_data_multiclass/test/images/" \
---test_gt_path "/home/vuedale/Desktop/sam2.1/Fine-Tuning-SAM2-UNet-for-Breast-Image-Segmentation-Without-Pectoral-Muscle-and-Artifacts/sam_data_multiclass/test/masks/" \
---save_path "/home/vuedale/Desktop/sam2.1/Fine-Tuning-SAM2-UNet-for-Breast-Image-Segmentation-Without-Pectoral-Muscle-and-Artifacts/sam_data_multiclass/test/predictions/" \
+--checkpoint "/home/yousof/Downloads/35epochs_0008lr_28batchsize_512inputsize.pth" \
+--test_image_path "/home/yousof/Desktop/samunet/samunet/new_data_coco_multiclass/train/images/" \
+--test_gt_path "/home/yousof/Desktop/samunet/samunet/new_data_coco_multiclass/train/masks/" \
+--save_path "/home/yousof/Desktop/samunet/samunet/new_data_coco_multiclass/train/predictions/" \
 --num_classes 7 \
 --input_size 512
